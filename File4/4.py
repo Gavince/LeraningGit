8bcf07564b5f9424ac7d1a92383cdf0a528f80cd
Gavince <1259781128@qq.com>
2022-01-11 14:48:40 +0800

    .ignore文件测试

---
+++ File4/4.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/1/11 下午2:46
+# @Author  : gavin
+# @FileName: 4.py
+# @Software: PyCharm
+# @Blog    ：https://blog.csdn.net/weixin_35154281
---
+++ File5/File5.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/1/11 下午2:47
+# @Author  : gavin
+# @FileName: File5.py
+# @Software: PyCharm
+# @Blog    ：https://blog.csdn.net/weixin_35154281
